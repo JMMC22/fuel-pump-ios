183164160c996ac770d0828bd95ec32c50afef1f
José María <jmmarquezcrepo@gmail.com>
2023-01-10 20:13:02 +0100

    Initial project

---
+++ FuelPump/FuelPump/FuelPumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelPumpApp.swift
+//  FuelPump
+//
+//  Created by Jose Mari on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FuelPumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
