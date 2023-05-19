61252031a3ac0099667a5a597254cb0b225eba15
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2023-05-19 18:09:46 +0200

    feat: adds fonts resources and typography modifier

---
+++ FuelPump/FuelPump/Presentation/DesignSystem/Fonts.swift
@@ -0,0 +1,45 @@
+//
+//  Fonts.swift
+//  FuelPump
+//
+//  Created by José María Márquez Crespo on 19/5/23.
+//
+
+import Foundation
+import SwiftUI
+
+enum FontType: String {
+    case inter
+
+    var name: String {
+        self.rawValue.capitalized
+    }
+}
+
+enum FontWeight: String {
+    case extraLight
+    case light
+    case thin
+    case regular
+    case medium
+    case semiBold
+    case bold
+    case extraBold
+    case black
+
+    var name: String {
+        "-" + self.rawValue.capitalized
+    }
+}
+
+extension Font {
+    static func font(type: FontType, weight: FontWeight, size: CGFloat) -> Font {
+        .custom(type.name + weight.name, size: size)
+    }
+}
+
+extension UIFont {
+    class func font(type: FontType, weight: FontWeight, size: CGFloat) -> UIFont {
+        return UIFont(name: type.name + weight.name, size: size)!
+    }
+}
