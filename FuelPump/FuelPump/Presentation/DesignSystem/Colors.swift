913c2fe07984b96099a9ae12bbec7a04a4706fce
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2023-07-06 19:37:31 +0200

    feat: adds primary color

---
+++ FuelPump/FuelPump/Presentation/DesignSystem/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  FuelPump
+//
+//  Created by José María Márquez Crespo on 6/7/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let primaryColor = Color("Primary")
+}
