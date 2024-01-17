605379d32478bde58d14a67eff5bf37474f17840
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2024-01-17 08:27:26 +0100

    feat: adds details empty view

---
+++ FuelPump/FuelPump/Presentation/Screens/GasStation/Details/GasStationDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  GasStationDetailsView.swift
+//  FuelPump
+//
+//  Created by José María Márquez Crespo on 17/1/24.
+//
+
+import SwiftUI
+
+struct GasStationDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GasStationDetailsView()
+}
